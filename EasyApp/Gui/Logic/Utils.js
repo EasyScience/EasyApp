bce47d17fb39f0a974b36e4eab4f0bd39fff8f21
Andrew Sazonov <andrew.sazonov@ess.eu>
2023-02-24 13:42:40 +0100

    Add read write file functions

--- EasyApp/Gui/Logic/Utils.js
+++ EasyApp/Gui/Logic/Utils.js
@@ -71,3 +71,44 @@ function isQmlScene()
     }
     return false
 }
+
+// XMLHttpRequest: Using PUT on a local file is disabled by default.
+// Set QML_XHR_ALLOW_FILE_WRITE to 1 to enable this feature.
+// E.g., 'QML_XHR_ALLOW_FILE_WRITE=1 qml main.qml'
+function writeFile(url, content) {
+    const method = "PUT"
+    const async = false
+    const request = new XMLHttpRequest()
+    request.onreadystatechange = () => {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                print(`Succeeded to write file: '${url}'`)
+            } else {
+                print(`Failed to write file: '${url}'. Status: ${request.status}`)
+            }
+        }
+    }
+    request.open(method, url, async)
+    request.send(content)
+}
+
+// XMLHttpRequest: Using GET on a local file is disabled by default.
+// Set QML_XHR_ALLOW_FILE_READ to 1 to enable this feature.
+// E.g., 'QML_XHR_ALLOW_FILE_READ=1 qml main.qml'
+function readFile(url) {
+    const method = "GET"
+    const async = false
+    const request = new XMLHttpRequest()
+    request.onreadystatechange = () => {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                print(`Succeeded to read file: '${url}'`)
+            } else {
+                print(`Failed to read file: '${url}'. Status: ${request.status}`)
+            }
+        }
+    }
+    request.open(method, url, async)
+    request.send()
+    return request.responseText
+}
